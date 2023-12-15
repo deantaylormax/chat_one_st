62f3d5687b1deaf9935cb4fe98513bcfb3574c47
John Taylor <johntaylormax@outlook.com>
2023-12-15 01:08:59 -0500

    auth added

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 
 # %%
 from dotenv import load_dotenv
+import os
+import dotenv
+dotenv.load_dotenv()
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
@@ -10,10 +13,44 @@ from langchain.chains.question_answering import load_qa_chain
 from langchain.llms import OpenAI
 from langchain.callbacks import get_openai_callback
 
+# Define your username and password
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+USER1 = os.getenv("user1")
+PASSWORD1 = os.getenv("password1")
+
+# Initialize session state for logged_in and attempted_login
+if 'logged_in' not in st.session_state:
+    st.session_state.logged_in = False
+if 'attempted_login' not in st.session_state:
+    st.session_state.attempted_login = False
+
+if not st.session_state.get('logged_in', False):
+    # User is not logged in, show login fields
+    username = st.sidebar.text_input("Username")
+    password = st.sidebar.text_input("Password", type="password")
+
+    if st.sidebar.button("Login"):
+        st.session_state.attempted_login = True
+        # Check against both sets of credentials
+        if (username == USER1 and password == PASSWORD1) or (username == USERNAME and password == PASSWORD):
+            st.session_state.logged_in = True
+            st.success("Logged in successfully!")
+        else:  # This else belongs to the for loop, not the if statement
+            if st.session_state.attempted_login:
+                st.error("Incorrect username or password")
+else:
+    # User is logged in, show logout button
+    if st.sidebar.button("Logout"):
+        st.session_state.logged_in = False
+        st.session_state.attempted_login = False
+        st.session_state.user_role = None
+        st.rerun()  # Use experimental_rerun for the latest versions of Streamlit
+
 def main():
     load_dotenv()
-    st.set_page_config(page_title="Talk to your PDF")
-    st.header("Ask your PDF 💬")
+    # st.set_page_config(page_title="Talk to your PDF")
+    st.header("Ask your PDF")
 
     # upload file
     pdf = st.file_uploader("Upload your PDF", type="pdf")
@@ -33,11 +70,9 @@ def main():
             length_function=len
         )
         chunks = text_splitter.split_text(text)
-
         # create embeddings
         embeddings = OpenAIEmbeddings()
         knowledge_base = FAISS.from_texts(chunks, embeddings)
-
         # show user input
         user_question = st.text_input("Ask a question about your PDF:")
         if user_question:
@@ -48,7 +83,8 @@ def main():
             with get_openai_callback() as cb:
                 response = chain.run(input_documents=docs, question=user_question)
                 print(cb)
-
             st.write(response)
-if __name__ == '__main__':
+if st.session_state.logged_in:
     main()
+# if __name__ == '__main__':
+#     main()
